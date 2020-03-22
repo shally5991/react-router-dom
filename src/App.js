536812823a8d96e8d1ede1ac6fd744fff83940da
shally5991 <shailja5991@gmail.com>
2020-03-22 17:51:14 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, {Component} from 'react';
 import './App.css';
+import { Route ,Link} from 'react-router-dom';
 
+const HomePage = (props) => {
+ 
+  return (
+
+    <div> 
+    <Link to = '/topics'>Topic</Link>
+    <h1>Home Page</h1>
+    </div>
+  );
+
+}
+
+const TopicsList = (props) => {
+  return (
+    <div>
+    <button onClick = {() => props.history.push('/')}>homepage</button>
+    <h1>Topic List</h1>
+    </div>
+
+  );
+
+}
+const TopicDetail = () => {
+  return (
+    <div>
+    <h1>Topic Detail Page</h1>
+    </div>
+  );
+
+}
 class App extends Component {
   render(){
     return(
       <div>
-      {'sample to learn deploy'}
+      <Route exact path ='/' component = {HomePage } />
+      <Route exact path = '/topics' component ={TopicsList} />
+      <Route path = '/topics/:topicID' component = {TopicDetail} />  
       </div>
-    )
+    );
   }
 }
 
