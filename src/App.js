c562553f16866a83819acd8f9f9052a2b1a87110
shally5991 <shailja5991@gmail.com>
2020-03-22 18:07:43 +0530

    ad

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import { Route ,Link} from 'react-router-dom';
 
-const HomePage = (props) => {
- 
-  return (
-
-    <div> 
-    <Link to = '/topics'>Topic</Link>
-    <h1>Home Page</h1>
-    </div>
-  );
-
-}
-
-const TopicsList = (props) => {
-  return (
-    <div>
-    <button onClick = {() => props.history.push('/')}>homepage</button>
-    <h1>Topic List</h1>
-    </div>
-
-  );
-
-}
-const TopicDetail = () => {
-  return (
-    <div>
-    <h1>Topic Detail Page</h1>
-    </div>
-  );
-
-}
 class App extends Component {
   render(){
     return(
       <div>
-      <Route exact path ='https://shally5991.github.io/react-router-dom/' component = {HomePage } />
-      <Route exact path = '/topics' component ={TopicsList} />
-      <Route path = '/topics/:topicID' component = {TopicDetail} />  
-      </div>
+      <h1> hi</h1>
+       </div>
     );
   }
 }
